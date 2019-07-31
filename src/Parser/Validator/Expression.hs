3894387fb201d75aad0658f341c004d22f0ec411
Howard Chen <hchen7913@gmail.com>
2019-07-31 14:32:50 -0700

    Begin adding typechecking for functions

--- src/Parser/Validator/Expression.hs
+++ src/Parser/Validator/Expression.hs
@@ -22,16 +22,44 @@ data Type
 validateExprTypes :: State ValidExprState Type 
 validateExprTypes = do 
     state <- get
-    return ( case expr state of
-        S.Identifier id -> Identifier
-        S.Constant prim -> case prim of 
-            S.Boolean _ -> Boolean
-            S.Number _ -> Number 
-            S.String _ -> String
-        S.SubQuery _ _ -> Subquery
-        S.Operator opType -> Number -- Fix 
-        S.Function id args -> Number -- Fix
-                )
+    return 
+        ( case expr state of
+            S.Identifier id -> Identifier
+            S.Constant prim -> case prim of 
+                S.Boolean _ -> Boolean
+                S.Number _ -> Number 
+                S.String _ -> String
+            S.SubQuery _ _ -> Subquery
+            S.Operator opType -> evalState validateOpTypes (ValidOpState { opErrors="", op=opType }) 
+            S.Function fid fargs -> evalState validateFnTypes (ValidFnState { fnErrors="", id=fid, args=fargs } )
+        )
+
+data ValidFnState = ValidFnState 
+    { fnErrors :: String 
+    , id :: String
+    , args :: S.Args
+    }
+
+validateFnTypes :: State ValidFnState Type
+validateFnTypes = do 
+    state <- get
+    let maybeFnInfo = Map.lookup ( id state ) fnMap
+
+fnMap :: Map.Map String OperatorInfo
+fnMap = Map.fromList 
+    [ ("COUNT", mkOperatorInfo Number [(=) Identifier] )
+    , ("AVG", mkOperatorInfo Number [(=) Identifier])
+    , ("SUM", mkOperatorInfo Number [(=) Identifier])
+    , ("MAX", mkOperatorInfo Number [(=) Identifier])
+    , ("MIN", mkOperatorInfo Number [(=) Identifier])
+    , ("CONCAT", mkOperatorInfo String [(=) String, (=) String])
+    , ("LENGTH", mkOperatorInfo Number [(=) String])
+    , ("LEFT", mkOperatorInfo Number [(=) String, (=) Number])
+    , ("REPLACE", mkOperatorInfo )
+    ]
+
+
+
 
 data ValidOpState = ValidOpState 
     { opErrors :: String
@@ -44,26 +72,38 @@ validateOpTypes = do
     state <- get
     let operator = op state
         args = operatorArgs operator
-        info = operatorInfo operator
-        typeCheckers = argTypes info
-    t <- return
-            (  
-                if (length typeCheckers == length args) && 
-                       (foldr (&&) True ( typeCheckers <*> (findType <$> args))) 
-                then (returnType info)
-                else Unknown        
-            )
+        infos = operatorInfo operator
+    t <- return (typeChecks infos (findType <$> args))
     return t
 
     where findType :: S.Expr -> Type
-          findType ex = evalState validateExprTypes (ValidExprState {exprErrors="", expr=ex })
+          findType ex = 
+                evalState validateExprTypes (ValidExprState {exprErrors="", expr=ex })
+          typeChecks :: [ OperatorInfo ] -> [ Type ] -> Type
+          typeChecks checkers types = 
+                foldr (selectType) Unknown ((typeCheck types) <$> checkers)
+          typeCheck :: [Type] -> OperatorInfo -> Type
+          typeCheck types info = case info of 
+                OperatorInfo i -> 
+                    if (length (argTypes i) == length args) && 
+                                (foldr (&&) True ( typeCheckers <*> (findType <$> args))) 
+                    then (returnType i)
+                    else Unknown
+                SameTwo st -> 
+                    if length types == 2 && (head types) == ( head . head $ types)
+                    then (returnType st)
+                    else Unknown
+          selectType :: Type -> Type -> Type 
+          selectType (Unknown) x = x
+          selectType x (Unknown) = x
+          selectType x y = y
 
 data OperatorInfo  
     = OperatorInfo
         { returnType :: Type
         , argTypes :: [ Type -> Bool]
         }
-    | SpecialInfo { r :: Type }
+    | SameTwo { returnType :: Type }
 
 mkOperatorInfo :: Type -> [Type -> Bool] -> OperatorInfo 
 mkOperatorInfo ret args = OperatorInfo 
@@ -71,25 +111,48 @@ mkOperatorInfo ret args = OperatorInfo
             , argTypes = args
             }
 
-operatorInfo :: S.OperatorType -> OperatorInfo
+operatorInfo :: S.OperatorType -> [OperatorInfo]
 operatorInfo op = case op of 
-    S.Plus _ _ ->  mkOperatorInfo Number [ (==) Number,  (==) Number]
-    S.Minus _ _ ->  mkOperatorInfo Number [ (==) Number,  (==) Number]
-    S.FloatDivide _ _ ->  mkOperatorInfo Number [ (==) Number,  (==) Number]
-    S.Multiply _ _ ->  mkOperatorInfo Number [ (==) Number,  (==) Number]
-    S.Modulo _ _ ->  mkOperatorInfo Number [ (==) Number,  (==) Number]
-    S.Equals _ _ ->  mkOperatorInfo Boolean [ (==) Number,  (==) Number]
-    S.NotEquals _ _ ->  mkOperatorInfo Boolean [ (==) Number,  (==) Number]
-    S.LT _ _ ->  mkOperatorInfo Boolean [ (==) Number,  (==) Number]
-    S.LTE _ _ ->  mkOperatorInfo Boolean [ (==) Number,  (==) Number]
-    S.GT _ _ ->  mkOperatorInfo Boolean [ (==) Number,  (==) Number]
-    S.GTE _ _ ->  mkOperatorInfo Boolean [ (==) Number,  (==) Number]
-    S.And _ _ ->  mkOperatorInfo Boolean [ (==) Boolean,  (==) Boolean]
-    S.Or _ _ ->  mkOperatorInfo Boolean [ (==) Boolean,  (==) Boolean]
-    S.Not _ ->  mkOperatorInfo Boolean [ (==) Boolean ]
-    S.Neg _ ->  mkOperatorInfo Number [(==) Number]
-    S.In _ _ ->  mkOperatorInfo Number [(==) Number, (==) Subquery]
-    S.NotIn _ _ ->  mkOperatorInfo Number [(==) Number, (==) Subquery]
+    S.Plus _ _ ->   [ mkOperatorInfo Number [ (==) Number,  (==) Number]
+                    ]
+    S.Minus _ _ ->  [ mkOperatorInfo Number [ (==) Number,  (==) Number]
+                    ]
+    S.FloatDivide _ _ ->    [ mkOperatorInfo Number [ (==) Number,  (==) Number]
+                            ]
+    S.Multiply _ _ ->   [ mkOperatorInfo Number [ (==) Number,  (==) Number]
+                        ]
+    S.Modulo _ _ ->  [ mkOperatorInfo Number [ (==) Number,  (==) Number]
+                    ]
+    S.Equals _ _ -> [ mkOperatorInfo Boolean [ (==) Number,  (==) Number] 
+                    , SameTwo { returnType = Boolean }
+                    ]
+    S.NotEquals _ _ ->  [ mkOperatorInfo Boolean [ (==) Number,  (==) Number]
+                        , SameTwo { returnType = Boolean }
+                        ]
+    S.LT _ _ ->  [ mkOperatorInfo Boolean [ (==) Number,  (==) Number]
+                 , mkOperatorInfo Boolean [ (==) String, (==) String ]
+                 ]
+    S.LTE _ _ ->    [ mkOperatorInfo Boolean [ (==) Number,  (==) Number]
+                    , mkOperatorInfo Boolean [ (==) String, (==) String ]
+                    ]
+    S.GT _ _ ->     [ mkOperatorInfo Boolean [ (==) Number,  (==) Number]
+                    , mkOperatorInfo Boolean [ (==) String, (==) String ]
+                    ]
+    S.GTE _ _ ->    [ mkOperatorInfo Boolean [ (==) Number,  (==) Number]
+                    , mkOperatorInfo Boolean [ (==) String, (==) String ]
+                    ]
+    S.And _ _ ->    [ mkOperatorInfo Boolean [ (==) Boolean,  (==) Boolean]
+                    ]
+    S.Or _ _ -> [ mkOperatorInfo Boolean [ (==) Boolean,  (==) Boolean]
+                ]
+    S.Not _ ->  [ mkOperatorInfo Boolean [ (==) Boolean ]
+                ]
+    S.Neg _ ->  [ mkOperatorInfo Number [(==) Number]
+                ]
+    S.In _ _ -> [ mkOperatorInfo Number [(==) Number, (==) Subquery]
+                ]
+    S.NotIn _ _ ->  [ mkOperatorInfo Number [(==) Number, (==) Subquery]
+                    ]
 
 operatorArgs :: S.OperatorType -> S.Args 
 operatorArgs op = case op of 
@@ -108,5 +171,5 @@ operatorArgs op = case op of
     S.Or op1 op2 ->  [op1, op2]
     S.Not op1 ->  [op1]
     S.Neg op1 ->  [op1]
-    S.In op1 _ ->  [op1]
-    S.NotIn op1 _ ->  [op1]
+    S.In op1 op2 ->  [op1, op2]
+    S.NotIn op1 op2 ->  [op1, op2]
