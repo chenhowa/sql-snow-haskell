eaae298f882da882674072177cfe50b2364ba0e2
Howard Chen <hchen7913@gmail.com>
2019-08-09 08:55:05 -0700

    Add code for pulling projections out of columns

--- src/Optimizer/Extractor/Projections.hs
+++ src/Optimizer/Extractor/Projections.hs
@@ -4,21 +4,43 @@ import qualified Optimizer.Extractor.Tables as T
 import qualified Parser.Syntax as P
 import qualified Data.List as L
 import Parser.Validator.Expression (operatorArgs)
+import qualified Data.Map.Strict as Map
 
-type ValidProjectionInfo = String
+type ValidProjectionInfo = Map.Map Source [Column]
 type Error = String
 
 
-validate :: Either Error ColumnStream -> Either Error ValidProjectionInfo
-validate either = undefined
+
+
+validate :: T.ValidTableInfo -> Either Error ColumnStream -> Either Error ValidProjectionInfo
+validate info either = case either of 
+    Left e -> Left e
+    Right cols -> foldr collect (Right $ Map.empty) cols
+
+    collect :: T.ValidTableInfo -> (Maybe Source, Column) -> Either Error ValidProjectionInfo -> Either Error ValidProjectionInfo
+    collect (tmap, amap) (msource, col) result = case result of 
+        Left _ -> result
+        Right pmap -> case msource of 
+            Nothing -> case Map.size tmap of  -- this should be done with guards, not case
+                        0 -> Left "There are no tables in the query for the column \'" <> col <> "\' to be part of"
+                        1 -> let newResult = case uncons $ Map.toList tmap of 
+                                    Just ((name, _), _) -> Right $ Map.insertWith (<>) name [col]
+                                    Nothing -> Left "Inconsistent table count"
+                             in  newResult
+                        _ -> Left "There are multiple tables in the query. Column \'" <> col <> "\' must be aliased"
+            Just src -> case Map.lookup src tmap of 
+                Just times -> if times  -- if table has been used more than once, it should have been aliased
+                              then Left $ "Table \'" <> src "\' occurs more than once in query. Its alias should be used to refer to it"
+                              else Right $ Map.insertWith (<>) name [col]
+                Nothing -- Then we need to look this up by alias, not table name
 
 
 type ColumnStream = [(Maybe Source, Column)]
 type Source = String
 type Column = String
 
-extractColumns :: T.ValidTableInfo -> P.SelectType -> Either Error ColumnStream 
-extractColumns info stype = case stype of 
+extractColumns :: P.SelectType -> Either Error ColumnStream 
+extractColumns stype = case stype of 
     P.Wildcard -> Left $ "Selecting all output columns with \'*\' is currently not allowed"     
     P.Columns cols -> Right (concat (convert <$> cols))
 
