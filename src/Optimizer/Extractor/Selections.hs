56f65dd678caeaf979bb1b3703b8fd4e00840693
Howard Chen <hchen7913@gmail.com>
2019-08-13 08:55:19 -0700

    Add more to pass selection tests

--- src/Optimizer/Extractor/Selections.hs
+++ src/Optimizer/Extractor/Selections.hs
@@ -5,6 +5,7 @@ import qualified Data.Map.Strict as Map
 import Parser.Validator.Expression (operatorArgs)
 import qualified Data.List as L
 import qualified Data.Char as Char
+import Optimizer.Extractor.Projections (tableAndColumn)
 
 {- As far as I can tell, the granularity of selections can be at any level ranging from one table, to all tables in the query.
 Consequently, it initially doesn't make sense to pair selections directly with table info. It *does* make sense to project 
@@ -21,7 +22,16 @@ data SelectionInfo = SelectionInfo
     , expr :: P.Expr
 
     }
-    deriving (Eq, Show)
+    deriving (Show)
+
+instance Eq SelectionInfo where 
+    (==) a b = 
+        let exprEquality = (expr a == expr b)
+            memberEquality = 
+                let map1 = Map.fromList $ fmap (flip (,) False) (tables a)
+                    map2 = Map.fromList $ fmap (flip (,) False) (tables b)
+                in map1 == map2
+        in exprEquality && memberEquality
 
 type Table = (Name, Maybe Alias)
 type Name = String
@@ -47,8 +57,10 @@ extract wh =
             case ex of 
                 P.Constant _ -> -- a constant doesn't change the tables, doesn't provoke abort, and it doesn't provide any selections
                     Right $ (map, False, [])
-                P.Identifier str -> -- an identifier adds a table, doesn't provoke abort, and doesn't provide selections
-                    Right $ (Map.insert str False map, False, [])
+                P.Identifier str -> 
+                    case tableAndColumn str of 
+                        Nothing -> Left $ "Support for column with no table/alias is not yet implemented"
+                        Just (src, col) -> Right $ (Map.insert src False map, False, []) -- an identifier adds a table, doesn't provoke abort, and doesn't provide selections
                 P.Function id args -> case functionLookup id of 
                     Nothing -> Left $ "Function \'" <> id <> "\' not recognized"
                     Just (FunctionInfo {returnType = rt}) ->  Left $ "stil need to implement" -- if it's a boolean, I suppose it qualifies as a selection we could do
@@ -59,10 +71,13 @@ extract wh =
                         let newMap = Map.union map1 map2
                         -- return the new map, tell above expressions to abort, and save the Or expression only without any subexpressions
                         return (newMap, True, [SelectionInfo { tables = fmap ((flip (,)) Nothing) $ Map.keys newMap, expr = ex}]) 
-                    P.And op1 op2 -> do
-                        (map1, _, _) <- extract_ either op1
-                        (map2, _, _) <- extract_ either op2
+                    P.And op1 op2 -> do -- And expressions do not generate their own expressions, since the selections on lower tables automatically perform them
+                        (map1, a1, s1) <- extract_ either op1
+                        (map2, a2, s2) <- extract_ either op2
                         let newMap = Map.union map1 map2
+                            newAbort = a1 || a2
+                        if newAbort
+                        then return (newMap, True)
                         return (newMap, False, [])
                     P.Equals op1 op2 -> comparison ex op1 op2 either
                     P.NotEquals op1 op2 -> comparison ex op1 op2 either
@@ -75,16 +90,20 @@ extract wh =
         comparison :: P.Expr -> P.Expr -> P.Expr -> Either Error (TablesUsed, Abort, SelectionStream) -> Either Error (TablesUsed, Abort, SelectionStream)
         comparison _ _ _ either@(Left _) = either
         comparison comp op1 op2 opts@(Right (emap, abort, stream)) = do 
-            (map1, _, str1) <- extract_ (opts) op1
-            (map2, _, str2) <- extract_ (opts) op2
+            (map1, a1, str1) <- extract_ (opts) op1
+            (map2, a2, str2) <- extract_ (opts) op2
             let newMap = Map.union map1 map2
-            if (Map.size $ newMap) == 1 -- if the tables below only had 1 table total, we can take the entire the selection down to the table used
-            then 
-                let newStream = [SelectionInfo { tables = fmap ((flip (,)) Nothing) $ Map.keys newMap, expr = comp }]
-                in  return (newMap, abort, newStream)
-            else  -- but if there are multiple tables, then this equality comparison needs to be appended to the other selections from lower down
-                let newStream = (SelectionInfo { tables = fmap ((flip (,)) Nothing) $ Map.keys newMap, expr = comp }):(str1 <> str2)
-                in  return (newMap, abort, newStream)
+                newAbort = a1 || a2
+            if newAbort 
+            then return (newMap, True, [SelectionInfo {tables = fmap ((flip (,)) Nothing) $ Map.keys newMap, expr = comp }])
+            else 
+                if (Map.size $ newMap) == 1 -- if the tables below only had 1 table total, we can take the entire the selection down to the table used
+                then 
+                    let newStream = [SelectionInfo { tables = fmap ((flip (,)) Nothing) $ Map.keys newMap, expr = comp }]
+                    in  return (newMap, abort, newStream)
+                else  -- but if there are multiple tables, then this equality comparison needs to be appended to the other selections from lower down
+                    let newStream = (SelectionInfo { tables = fmap ((flip (,)) Nothing) $ Map.keys newMap, expr = comp }):(str1 <> str2)
+                    in  return (newMap, abort, newStream)
 
 
 
